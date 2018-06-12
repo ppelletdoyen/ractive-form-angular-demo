9326bcae56fa62e10420f84da965412800b40c09
ppe <pascal.pellet-doyen@qiminfo.ch>
2018-06-12 13:57:01 +0200

    - simplify the userform uses ; - fix / removing the not appropriate map ;

--- src/app/pages/content/user-detail/user-detail.component.ts
+++ src/app/pages/content/user-detail/user-detail.component.ts
@@ -1,10 +1,10 @@
+import { User } from './../../../shared/model/user';
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-import { User } from '../../../shared/model/user';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserApiService } from '../../../core/services/api/user-api.service';
 import { switchMap, map } from 'rxjs/operators';
 import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user-detail',
@@ -14,36 +14,32 @@ import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms'
 export class UserDetailComponent implements OnInit {
 
   paramsEvents: Subscription;
-  userEvents: Subscription;
   userDetail: User;
   userForm: FormGroup;
-  nameCtrl: FormControl;
 
   constructor(private activeRoute: ActivatedRoute, private userService: UserApiService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
-    this.nameCtrl = this.fb.control('', [Validators.required]);
     this.userForm = this.fb.group({
-      name: this.nameCtrl
+      name: ['', [Validators.required]]
     });
     this.paramsEvents = this.activeRoute.params.pipe(
       switchMap(param => this.userService.get(param.id)),
-      map(res => this.userDetail = res)
     )
-    .subscribe(res => this.nameCtrl.setValue(res.name));
+    .subscribe(res => {
+      this.buildUserForm(res);
+    })
   }
 
   onDestroy() {
     if (this.paramsEvents) {
       this.paramsEvents.unsubscribe();
     }
-    if (this.userEvents) {
-      this.userEvents.unsubscribe();
-    }
   }
 
   save() {
-    this.userEvents = this.userService.save(new User(this.userDetail.id, this.nameCtrl.value)).subscribe(res => {
+    const saveUser: User = this.prepareSaveUser();
+    this.userService.save(saveUser).subscribe(res => {
       this.router.navigate(['/users']);
     }, (err) => {
       console.error(err);
@@ -52,6 +48,22 @@ export class UserDetailComponent implements OnInit {
 
   cancel() {
     this.router.navigate(['/users']);
+  }
+
+  private buildUserForm(userDetail: User) {
+    this.userDetail = userDetail;
+    this.userForm.reset({
+      name: this.userDetail.name
+    });
+  }
+
+  private prepareSaveUser(): User {
+    const formModel = this.userForm.value;
+    const saveUser: User = {
+      id: this.userDetail.id,
+      name: formModel.name
+    }
+    return saveUser;
   }
 
 }
--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -1,6 +1,6 @@
 export class User {
-    public id: number;
-    public name: string;
+    id: number;
+    name: string;
     constructor(id?: number, name?: string) {
         this.id = id;
         this.name = name;
